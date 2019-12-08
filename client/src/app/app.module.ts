9df0cb36dcd2725ec35ea8cbfb2155ea46b74ae0
AsianJohnBoi <Jbssantias@hotmail.com>
2019-12-08 17:47:24 +1000

    form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatOptionModule,
+  MatSelectModule
+} from '@angular/material';
+import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/form/form.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import axios from 'axios';
+import {FormControl, FormGroupDirective, FormGroup, NgForm, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+  myForm = new FormGroup({
+    title: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    body: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    category: new FormControl('', [
+      Validators.required
+    ]),
+    subject: new FormControl('', []),
+  });
+    
+
+  onSubmit() {
+    console.log("submit!", this.myForm.value);
+    axios({
+      method: 'post',
+      url: '/',
+      data: this.myForm.value
+    })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+}
